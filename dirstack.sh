f9d1a17fe4d1534c6f4c51889994bb7a6173a5e2
Zac Joffe <zacharyjoffe@gmail.com>
2020-03-27 21:10:44 -0400

    Use fzf to select dir to pop

--- dirstack.sh
+++ dirstack.sh
@@ -6,6 +6,7 @@ push () {
 	if [[ ! -e $DIR_STACK ]]; then
 		echo -e "$PWD" > $DIR_STACK
 	else
+		# only push if the directory is different
 		if ! grep -Fxq "$PWD" $DIR_STACK; then
 			echo -e "$PWD\n$(cat $DIR_STACK)" > $DIR_STACK	
 		fi
@@ -14,7 +15,11 @@ push () {
 
 pop () {
 	if [[ -e $DIR_STACK ]]; then
-		dir=$(head -n 1 $DIR_STACK)
+		if [[ "$1" == "list" ]]; then
+			dir=$(cat $DIR_STACK | fzf)
+		else
+			dir=$(head -n 1 $DIR_STACK)
+		fi
 
 		# only pop if the directory is different
 		if [[ $PWD != $dir ]]; then
@@ -22,3 +27,4 @@ pop () {
 		fi
 	fi
 }
+
