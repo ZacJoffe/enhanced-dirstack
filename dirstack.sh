bd91f9159c25a39f43a7870c21fcf6e5a195f796
Zac Joffe <zacharyjoffe@gmail.com>
2020-03-27 21:04:22 -0400

    Smart popping

--- dirstack.sh
+++ dirstack.sh
@@ -4,7 +4,7 @@ DIR_STACK="/tmp/dirstack"
 
 push () {
 	if [[ ! -e $DIR_STACK ]]; then
-		echo -e "$PWD\n" > $DIR_STACK
+		echo -e "$PWD" > $DIR_STACK
 	else
 		if ! grep -Fxq "$PWD" $DIR_STACK; then
 			echo -e "$PWD\n$(cat $DIR_STACK)" > $DIR_STACK	
@@ -14,6 +14,11 @@ push () {
 
 pop () {
 	if [[ -e $DIR_STACK ]]; then
-		cd $(head -n 1 $DIR_STACK)
+		dir=$(head -n 1 $DIR_STACK)
+
+		# only pop if the directory is different
+		if [[ $PWD != $dir ]]; then
+			cd $dir
+		fi
 	fi
 }
